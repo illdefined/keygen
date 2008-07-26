fdd744d5dfbb4def6c26a8c2e0e45596e48bf69d
Maximilian Gaß <mxey@cloudconnected.org>
2008-07-26 23:14:03 +0200

    generator/xmodmap.lua: Added padding
    Actually making this useful as a beautifier for xmodmap

--- generator/xmodmap.lua
+++ generator/xmodmap.lua
@@ -1,6 +1,6 @@
 function xmodmap(keys)
   for code, syms in pairs(keys) do
-    io.write(string.format("keycode %d = %s %s %s %s\n", code, syms.normal,
+    io.write(string.format("keycode %3d = %20s %20s %20s %20s\n", code, syms.normal,
      syms.shift, syms.ms, syms.ms_shift))
     end
   end
