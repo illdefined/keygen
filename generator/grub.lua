e630313c34e54988d716c78e2b5bba377959ab59
Maximilian Gaß <mxey@cloudconnected.org>
2008-07-27 00:34:30 +0200

    generator/grub.lua: Added shift mappings

--- generator/grub.lua
+++ generator/grub.lua
@@ -2,9 +2,14 @@ local qwerty = require("qwerty_map")
 
 function grub(keys)
   for code, syms in pairs(keys) do
-    io.write(string.format("setkey %s %s\n", syms.normal, 
-     qwerty[tonumber(code)].normal))
+    for i, v in ipairs({'normal', 'shift'}) do
+      sym_in = syms[v]
+      sym_grub = qwerty[tonumber(code)][v]
+      if sym_in ~= sym_grub then
+        io.write(string.format("setkey %s %s\n", sym_in, sym_grub))
+      end
     end
   end
+end
 
 return grub
